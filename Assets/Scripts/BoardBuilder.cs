485747273fb5748bc1a3e78f9b2e54550e56d8a6
Jason <jasondcaterino@gmail.com>
2018-11-13 20:09:53 -0600

    Initial commit

---
+++ Assets/Scripts/BoardBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BoardBuilder : MonoBehaviour
+{
+    public GameObject waterTilePrefab;
+    public GameObject addressTilePrefab;
+    public GameObject canvas;
+    public Text addressTileText;
+    float id;
+
+    // Use this for initialization
+    void Start()
+    {
+        buildBoard();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void buildBoard()
+    {
+
+        
+        float y = transform.position.y;
+        for (int j = 0; j < 10; j++)
+        {
+            float x = transform.position.x;
+            for (int i = 0; i < 10; i++)
+            {
+                Vector3 position = new Vector3(x, y, 0);
+                GameObject waterTile = Instantiate(waterTilePrefab, position, Quaternion.identity);
+                waterTile.transform.parent = canvas.transform;
+                x += (float).5;
+
+
+            }
+            y += (float).5;
+        }
+    }
+    
+
+}
---
+++ Assets/Scripts/FireMissle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FireMissle : MonoBehaviour {
+    bool hasShip = false;
+   
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void fireMissle(Tile tile)
+    {
+        if (hasShip)
+        {
+            tile.GetComponent<Image>().color = Color.red;
+            
+        }
+        else
+        {
+            tile.GetComponent<Image>().color = Color.black;
+        }
+        
+    }
+
+
+}
